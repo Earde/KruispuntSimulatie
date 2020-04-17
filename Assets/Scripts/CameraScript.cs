a2f0e28671d7225233bf28e0eb7bf723c41bbbfd
Earde Bearda <eardelb@hotmail.com>
2020-04-18 00:55:36 +0200

    update
    update

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform objectPosition;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = objectPosition.position + offset;
+        transform.LookAt(objectPosition);
+    }
+}
---
+++ Assets/Scripts/TrafficSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficSpawner : MonoBehaviour
+{
+    private void Start()
+    {
+        InvokeRepeating("SendTraffic", 0.0f, 1.0f);
+    }
+
+    private void SendTraffic()
+    {
+
+    }
+}
