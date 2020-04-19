f705fe4d7b08011725526cf973a965d4fb357d11
Earde Bearda <eardelb@hotmail.com>
2020-04-19 03:40:34 +0200

    update
    update

---
+++ Assets/Scripts/PathRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathRenderer : MonoBehaviour
+{
+    public Color lineColor;
+
+    private List<Transform> nodes = new List<Transform>();
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = lineColor;
+
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+
+        for (int i = 0; i < pathTransforms.Length; i++)
+        {
+            if (pathTransforms[i] != transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            Gizmos.DrawWireSphere(nodes[i].position, 0.3f);
+            if (i < nodes.Count - 1)
+            {
+                Gizmos.DrawLine(nodes[i].position, nodes[i + 1].position);
+            }
+        }
+    }
+}
--- Assets/Scripts/TrafficLight.cs
+++ Assets/Scripts/TrafficLight.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class TrafficLight : MonoBehaviour
 {
-    public Light light;
+    public Light trafficLight;
 
     // Start is called before the first frame update
     void Start()
     {
-        light.enabled = true;
+        trafficLight.enabled = true;
         SetRed();
     }
 
     public void SetRed()
     {
-        light.color = Color.red;
-        light.transform.localPosition = new Vector3(0f, 1f, -0.75f);
+        trafficLight.color = Color.red;
+        trafficLight.transform.localPosition = new Vector3(0f, 1f, -0.75f);
     }
 
     public void SetOrange()
     {
-        light.color = Color.yellow;
-        light.transform.localPosition = new Vector3(0f, 0f, -0.75f);
+        trafficLight.color = Color.yellow;
+        trafficLight.transform.localPosition = new Vector3(0f, 0f, -0.75f);
     }
 
     public void SetGreen()
     {
-        light.color = Color.green;
-        light.transform.localPosition = new Vector3(0f, -1f, -0.75f);
+        trafficLight.color = Color.green;
+        trafficLight.transform.localPosition = new Vector3(0f, -1f, -0.75f);
     }
 }
