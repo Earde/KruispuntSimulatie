50bd2e2b61da88e90b132f1aa4e8c4b893b481bd
Earde Bearda <eardelb@hotmail.com>
2020-04-26 06:35:10 +0200

    .
    .

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public Transform objectPosition;
-    public Vector3 offset;
-    public 
+    public Vector3 offset = Vector3.zero;
+    public float speed = 2.0f;
 
     void Start()
     {
@@ -16,6 +16,6 @@ public class CameraScript : MonoBehaviour
     void Update()
     {
         transform.LookAt(objectPosition);
-        transform.Translate(Vector3.right * Time.deltaTime * 2f);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
     }
 }
